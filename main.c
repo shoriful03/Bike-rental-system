902fff2dc68237a98f593d3e48185e72256ae813
shoriful03 <136871909+shoriful03@users.noreply.github.com>
2023-06-22 23:11:28 +0600

    splash screen added

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+
+void Splash_Screen() {
+    FILE*ascii= fopen("ascii.txt", "r");
+    char c;
+    if (ascii == NULL) {
+        printf("Failed to open ascii artsaL file.\n");
+        return;
+    }
+
+    while ((c = fgetc(ascii)) != EOF) {
+        putchar(c);
+    }
+
+    fclose(ascii);
+    sleep(4);   // Display the splash screen for 4 seconds
+    system("cls");    // Clear the screen
+
+}
+
+int main() {
+    Splash_Screen();
+
+
+    return 0;
+}
